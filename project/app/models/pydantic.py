53359023f02ed1b796a86064d5c33ff1ea9997da
ajisamudra <aji.samudra@traveloka.com>
2022-02-15 10:16:11 +0700

    add delete and put routes

--- project/app/models/pydantic.py
+++ project/app/models/pydantic.py
@@ -9,3 +9,6 @@ class SummaryPayloadSchema(BaseModel):
 
 class SummaryResponseSchema(SummaryPayloadSchema):
     id: int
+
+class SummaryUpdatePayloadSchema(SummaryPayloadSchema):
+    summary: str
--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -3,6 +3,8 @@
 import json
 
 
+### POST Route
+
 def test_create_summary(test_app_with_db):
     response = test_app_with_db.post(
         "/summaries/", data=json.dumps({"url": "https://foo.bar"})
@@ -25,6 +27,7 @@ def test_create_summaries_invalid_json(test_app):
         ]
     }
 
+### GET Route
 
 def test_read_summary(test_app_with_db):
     response = test_app_with_db.post(
@@ -59,3 +62,93 @@ def test_read_all_summaries(test_app_with_db):
 
     response_list = response.json()
     assert len(list(filter(lambda d: d["id"] == summary_id, response_list))) == 1
+
+### DELETE Route
+
+def test_remove_summary(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.delete(f"/summaries/{summary_id}/")
+    assert response.status_code == 200
+    assert response.json() == {"id": summary_id, "url": "https://foo.bar"}
+
+def test_remove_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.delete("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+### PUT Route
+
+def test_update_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+    )
+    assert response.status_code == 200
+
+    response_dict = response.json()
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["summary"] == "updated!"
+    assert response_dict["created_at"]
+
+
+def test_update_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.put(
+        "/summaries/999/",
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+    )
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+def test_update_summary_invalid_json(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/",
+        data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(f"/summaries/{summary_id}/", data=json.dumps({}))
+
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "url"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
+            {
+                "loc": ["body", "summary"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            }
+        ]
+    }
+
+def test_update_summary_invalid_keys(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/",
+        data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(f"/summaries/{summary_id}/", data=json.dumps({"url": "https://foo.bar"}))
+
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "summary"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            }
+        ]
+    }
+
